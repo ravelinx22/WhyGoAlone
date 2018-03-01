9c77bccef7decd9840c32a81cbd2c184290e4178
William Ravelo <drummerwilliam@gmail.com>
2018-02-28 20:58:31 -0500

    added bootstrap and reactstrap

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,6 @@ module.exports = {
 		{
 			test: /\.css$/,
 			loader: "style-loader",
-			exclude: /node_modules/
 		},
 		{	
 			test: /\.css$/,
@@ -28,10 +27,33 @@ module.exports = {
 				modules: true,
 			    localIdentName: '[name]__[local]___[hash:base64:5]'
 			},
-			exclude: /node_modules/
+		},
+	    { 
+			test: /\.png$/, 
+			loader: "url-loader?limit=100000" 
+		 },
+		{ 
+			test: /\.jpg$/, 
+			loader: "file-loader" 
+		},
+		{
+			test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+			loader: 'url?limit=10000&mimetype=application/font-woff'
+		},
+		{
+			test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+			loader: 'url?limit=10000&mimetype=application/octet-stream'
+		},
+		{
+			test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+			loader: 'file'
+		},
+		{
+			test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+			loader: 'url?limit=10000&mimetype=image/svg+xml'
 		}]
 	},
 	devServer: {
-		contentBase: path.join(__dirname, "client")
+		contentBase: path.resolve(__dirname, "client")
 	}
 }
