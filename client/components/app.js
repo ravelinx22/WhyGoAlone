10c7665bb33fdf8900131d6f94902e9af7a8c5a6
William Ravelo <drummerwilliam@gmail.com>
2018-03-02 22:16:57 -0500

    fixed reactstrap loading issue with webpack

--- client/components/app.js
+++ client/components/app.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import '../styles/styles.css'
 
-export default class App extends React.Component {
-  render () {
-    return <h1>hello</h1>;
+export default  class App extends React.Component {
+  render() {
+    return (
+      <div>Hello World </div>
+    );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -18,40 +18,8 @@ module.exports = {
 		},
 		{
 			test: /\.css$/,
-			loader: "style-loader",
-		},
-		{	
-			test: /\.css$/,
-			loader: "css-loader",
-			query: {
-				modules: true,
-			    localIdentName: '[name]__[local]___[hash:base64:5]'
-			},
-		},
-	    { 
-			test: /\.png$/, 
-			loader: "url-loader?limit=100000" 
-		 },
-		{ 
-			test: /\.jpg$/, 
-			loader: "file-loader" 
-		},
-		{
-			test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
-			loader: 'url?limit=10000&mimetype=application/font-woff'
-		},
-		{
-			test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
-			loader: 'url?limit=10000&mimetype=application/octet-stream'
-		},
-		{
-			test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
-			loader: 'file'
-		},
-		{
-			test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
-			loader: 'url?limit=10000&mimetype=image/svg+xml'
-		}]
+			loader:[ 'style-loader', 'css-loader' ]
+		 }]
 	},
 	devServer: {
 		contentBase: path.resolve(__dirname, "client")
