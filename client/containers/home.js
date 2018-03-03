a80e94d5e5db04c8ff6f25e454bf33aadc5134fa
William Ravelo <drummerwilliam@gmail.com>
2018-03-03 13:35:16 -0500

    finished home header image

--- client/containers/home.js
+++ client/containers/home.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import '../styles/styles.css'
+import '../styles/styles.css';
+import { HomeHeader } from '../components/home_header';
+import { Container, Row, Col } from 'reactstrap';
 
 export default class Home  extends React.Component {
   render() {
     return (
-      <div>
-		Home
-     </div>
+		<Container fluid="true" className="home_header_img">
+			<HomeHeader/>
+		</Container>
     );
   }
 }
