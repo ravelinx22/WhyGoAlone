f49e4d9dee358ef6df3a18a49741b9421f5e32af
William Ravelo <drummerwilliam@gmail.com>
2018-03-02 23:15:49 -0500

    removed unnecessary props and constructors in app.js

--- client/containers/app.js
+++ client/containers/app.js
@@ -7,19 +7,6 @@ import {
 import '../styles/styles.css'
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
   render() {
     return (
       <div>
