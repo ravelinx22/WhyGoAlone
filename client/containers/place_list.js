a436e34aa56ad8b73e348f3c9b8710a21681701c
William Ravelo <drummerwilliam@gmail.com>
2018-03-04 00:31:53 -0500

    started connecting views with backend

--- client/containers/place_list.js
+++ client/containers/place_list.js
@@ -5,6 +5,29 @@ import { Container, Row, Col } from 'reactstrap';
 import { PlaceHeaderList } from '../components/place_header_list.js';
 
 export default class PlaceList  extends React.Component {
+  constructor(props) {
+	super(props);
+	this.state = {
+		places: [],
+	}
+  }
+
+  componentDidMount() {
+	  fetch("http://localhost:8000/api/venues/category/4d4b7104d754a06370d81259?ll=40.7243,-74.0010")
+	  .then(results => {
+		return results.json();
+	  }).then(data => {
+		let places = data.venues.map((venue) => {
+			console.log(venue);
+			return(
+				<PlaceItem place_id={venue.id} name={venue.name} where={venue.location.address} link_url="/place" key={venue.id}/>
+			);
+		});
+
+		this.setState({places: places});
+	  });
+  }
+
   render() {
     return (
       <div className="place_list">
@@ -13,15 +36,7 @@ export default class PlaceList  extends React.Component {
 		</Container>
 		<Container>
 			<Row>
-				<PlaceItem name="Nombre" where="Buenas" link_url="/place"/>
-				<PlaceItem name="Nombre" where="Buenas" link_url="/place"/>
-				<PlaceItem name="Nombre" where="Buenas" link_url="/place"/>
-				<PlaceItem name="Nombre" where="Buenas" link_url="/place"/>
-				<PlaceItem name="Nombre" where="Buenas" link_url="/place"/>
-				<PlaceItem name="Nombre" where="Buenas" link_url="/place"/>
-				<PlaceItem name="Nombre" where="Buenas" link_url="/place"/>
-				<PlaceItem name="Nombre" where="Buenas" link_url="/place"/>
-				<PlaceItem name="Nombre" where="Buenas" link_url="/place"/>
+				{this.state.places}
 			</Row>
 		</Container>
      </div>
